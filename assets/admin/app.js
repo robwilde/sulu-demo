99921a65f00bb9fbf30340c378095cd57d441a23
Niklas Natter <niklas@sulu.io>
2020-10-06 16:37:22 +0200

    Implement currency content type

--- assets/admin/app.js
+++ assets/admin/app.js
@@ -1,1 +1,5 @@
 // Add project specific javascript code here:
+import {fieldRegistry} from 'sulu-admin-bundle/containers';
+import Currency from "./fields/Currency";
+
+fieldRegistry.add('currency', Currency);
