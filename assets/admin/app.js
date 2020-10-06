e0c05b6318206ce44cbca2696672a84a12145198
Niklas Natter <niklas@sulu.io>
2020-10-06 14:53:02 +0200

    Implement app.contact_statistics view

--- assets/admin/app.js
+++ assets/admin/app.js
@@ -1,1 +1,5 @@
 // Add project specific javascript code here:
+import {viewRegistry} from 'sulu-admin-bundle/containers';
+import ContactStatistics from "./views/ContactStatistics";
+
+viewRegistry.add('app.contact_statistics', ContactStatistics);
