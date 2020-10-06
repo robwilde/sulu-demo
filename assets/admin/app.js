2bae14a85a416ef737e12c0460e7ae58253bf17c
Niklas Natter <niklas@sulu.io>
2020-10-06 17:30:48 +0200

    Implement colored_text field transformer

--- assets/admin/app.js
+++ assets/admin/app.js
@@ -1,1 +1,5 @@
 // Add project specific javascript code here:
+import {listFieldTransformerRegistry} from 'sulu-admin-bundle/containers';
+import ColoredTextFieldTransformer from "./fieldTransformers/ColoredTextFieldTransformer";
+
+listFieldTransformerRegistry.add('colored_text', new ColoredTextFieldTransformer());
