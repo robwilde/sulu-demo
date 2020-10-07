596fde8407e1bc5ab52098521c01cd105ddc9ca7
Niklas Natter <niklas@sulu.io>
2020-10-07 15:40:59 +0200

    Implement app.generate_name form toolbar action

--- assets/admin/app.js
+++ assets/admin/app.js
@@ -1,1 +1,5 @@
 // Add project specific javascript code here:
+import {formToolbarActionRegistry} from 'sulu-admin-bundle/views';
+import GenerateNameToolbarAction from "./formToolbarActions/GenerateNameToolbarAction";
+
+formToolbarActionRegistry.add('app.generate_name', GenerateNameToolbarAction);
