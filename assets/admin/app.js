7d1e0c14095f129905d24ccb5a636b59a1ff0eea
Niklas Natter <niklas@sulu.io>
2020-10-23 16:48:53 +0200

    Implement app.alert_name list item action

--- assets/admin/app.js
+++ assets/admin/app.js
@@ -1,1 +1,5 @@
 // Add project specific javascript code here:
+import {listItemActionRegistry} from 'sulu-admin-bundle/views';
+import AlertNameItemAction from "./listItemActions/AlertNameItemAction";
+
+listItemActionRegistry.add('app.alert_name', AlertNameItemAction);
